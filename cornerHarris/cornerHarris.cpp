a128a03bfae27dbf08c5ad8a40ddbd356a6b8226
jianyongwang <jianyongwang_shu@163.com>
2014-12-29 19:19:52 +0800

    master1

--- cornerHarris/cornerHarris.cpp
+++ cornerHarris/cornerHarris.cpp
@@ -7,13 +7,7 @@ using namespace cv;
 int main()
 {
 	Mat image = imread("..\\..\\church01.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-	/*Mat cornerStrength;
-	cornerHarris(image, cornerStrength, 3, 3, 0.01);
-	Mat harrisCorner;
-	Mat harrisCorner1;
-	double threshod = 0.0001;
-	cv::threshold(cornerStrength, harrisCorner, threshod, 255, cv::THRESH_BINARY);
-	cv::threshold(harrisCorner, harrisCorner1, 30, 255, cv::THRESH_BINARY_INV);*/
+
 	HarrisDetector harris;
 	harris.detect(image);
 	vector<Point> ptrs;
